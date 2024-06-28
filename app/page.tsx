390ac07777fb97a80f09487ace73124e9dc29d9f
Redari-Es <905422030@qq.com>
2024-06-28 11:23:30 +0800

    nextjs-flexibble

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+
+      <h1>Categroies</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </div>
+  )
+}
+
+export default Home
